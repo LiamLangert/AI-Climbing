32900f85e626379938820e05ff5736db3dddca72
danielblauvelt <106919656+danielblauvelt@users.noreply.github.com>
2023-12-15 17:19:17 -0500

    Update script.py

--- src/script.py
+++ src/script.py
@@ -1,13 +1,13 @@
-
 import argparse
 import get_holds
 import shutil 
 from get_holds import getHoldsArray
 import importlib
 importlib.reload(get_holds)
+import Route_Finder
+from Route_Finder import uniformCostSearch, Limb, State, Person, RouteFinder, Route, moveToText, LimbName 
 
-
-def main(file, color):
+def main(file, color, height):
 #     - Red: 0 20 30 10 
 # - Yellow: 60 20 30 40 
 # - Green: 120 10 20 40 
@@ -50,7 +50,7 @@ def main(file, color):
     source = file
     
     # Destination path 
-    destination = '../images/traverse.jpg'
+    destination = '../images/'
     
     # Move the content of 
     # source to destination 
@@ -63,6 +63,24 @@ def main(file, color):
     print(holds)
     print(len(holds))
 
+
+    route = Route(holds = holds, start1 = holds[10], start2 = holds[10], finish = holds[0])
+    lh = Limb(LimbName.LEFT_HAND, 2.5, 8, route.start_hold1)
+    rh = Limb(LimbName.RIGHT_HAND, 2.5, 8, route.start_hold2)
+    lf = Limb(LimbName.LEFT_LEG, 8, 5, route.holds[-1])
+    rf = Limb(LimbName.RIGHT_LEG, 8, 5, route.holds[-1])
+    human = Person(height)
+
+    state1 = State(lf, rf, lh, rh, human, route)
+    a_srftar1 = RouteFinder(state=state1)
+    results1 = rf.uniformCostSearch()
+
+    for action in results1:
+        print(moveToText(action, route))
+
+    print(len(holds))
+    for i in range(len(holds)):
+        print(f"{i}: {holds[i].x}, {holds[i].y}, {holds[i].diff}")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -80,8 +98,19 @@ if __name__ == '__main__':
         required=True,
         type=str
     )
+
+    parser.add_argument(
+        '-h',
+        '--height',
+        help='height in inches',
+        required=True,
+        type=int
+    )
     args = parser.parse_args()
 
     file = str(args.file)
     color = str(args.color)
-    main(file, color)
+    height = int(args.height)
+    main(file, color, height)
+
+  
